d36c78826802659652888550e3ae7afe901aaedb
BDS Conculting <sumittarwey2268@gmail.com>
2020-04-11 10:45:58 +0530

    change in setting file

--- movie/settings.py
+++ movie/settings.py
@@ -75,16 +75,13 @@ WSGI_APPLICATION = 'movie.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'movie',
-        'USER':'root',
-        'PASSWORD':'valindex',
-        'PORT':'5432',
-        'OPTIONS': {
-            'init_command': 'SET innodb_strict_mode=1',
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
-        },  
+     'default': {
+        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'sqlite3.db',                      # Or path to database file if using sqlite3.
+        'USER': '',                      # Not used with sqlite3.
+        'PASSWORD': '',                  # Not used with sqlite3.
+        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
 }
 
